37d13cb1192b02f53ed77bf4c71d6cf2c3b9f345
Mojtaba-Khodakhah <mkhodakhah1991@gmail.com>
2025-09-16 11:03:54 +0330

    SC

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ['cdn-api.webofen.com'], // ← اینجا دامنه CDN خودت رو اضافه کن
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cms.webofen.com",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-api.webofen.com",
+        pathname: "/uploads/**",
+      },
+    ],
   },
 };
 
