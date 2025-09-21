a440476b561178009882662d66344e8eeffc1dcb
Mojtaba-Khodakhah <mkhodakhah1991@gmail.com>
2025-09-21 15:13:54 +0330

    SC

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -444,7 +444,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { slug } = ctx.params as { slug: string };
   let product: Product | null = null;
   try {
-    const res = await fetch(`http://localhost:3002/api/proxy/productbyslug/${slug}`, {
+    const res = await fetch(`http://localhost:3000/api/proxy/productbyslug/${slug}`, {
       headers: {
         "Content-Type": "application/json",
       },
