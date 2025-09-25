4113adeec0df72f066eeb31022ba2e4d56b4591b
avinmnf <avin.mnf@gmail.com>
2025-09-25 12:58:47 +0330

    logout

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -3,19 +3,8 @@ import { deleteCookie } from 'cookies-next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
- deleteCookie("token", {
-  req,
-  res,
-  path: "/",
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  domain: process.env.NODE_ENV === "production" ? ".yourdomain.com" : undefined,
-});
-
-
+    deleteCookie("token", { req, res, path: "/" });
     return res.status(200).json({ message: 'Logged out successfully' });
   }
-
   return res.status(405).json({ message: 'Method Not Allowed' });
 }
