7a584b4eaf6807cbe0cb67f56ea3e031a712035b
avinmnf <avin.mnf@gmail.com>
2025-10-07 15:27:25 +0330

    res

--- components/reservetime.tsx
+++ components/reservetime.tsx
@@ -40,70 +40,70 @@ export default function Reservetime() {
     }
   };
 
+
   return (
-    <motion.div
+      <motion.div
       className="relative w-full aspect-[3/4] sm:aspect-[15/3] overflow-hidden flex items-start"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: 'easeOut' }}
     >
-      <div className="relative w-full aspect-[3/4] sm:aspect-[15/3] overflow-hidden flex items-start">
-        {/* Desktop Image */}
-        <Image
-          src="/homepage/timereserve.png"
-          alt="Desktop slide"
-          fill
-          className="object-cover hidden sm:block"
-        />
-        {/* Mobile Image */}
-        <Image
-          src="/homepage/timeresmobile.png"
-          alt="Mobile slide"
-          fill
-          className="object-cover block sm:hidden"
-        />
-
-        <div className="absolute top-50 md:top-6 right-4 items-start z-10 p-10 md:p-4 pt-10 rounded-xl">
-          <div className="mt-10">
-            <p className="text-lg md:text-3xl text-white mb-4">
-              قبل از ثبت نوبت می‌توانید از ما
-              <span className="text-[#1d546b] font-semibold"> مشاوره رایگان </span>
-              دریافت کنید :
-            </p>
-          </div>
-
-          {/* Input Fields + Button */}
-          <div className="flex gap-4 w-full items-center mt-10">
-            <input
-              type="text"
-              placeholder="نام و نام خانوادگی"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="flex-1 bg-[#f7f8fc] text-gray-700 rounded-lg p-2 px-4 text-sm border border-transparent focus:outline-none focus:border-[#1d546b] focus:ring-2 focus:ring-[#1d546b] transition-all duration-200"
+        <div className="relative w-full aspect-[3/4] sm:aspect-[15/3] overflow-hidden flex items-start">
+            {/* Desktop Image */}
+            <Image
+                src="/homepage/timereserve.png"
+                alt="Desktop slide"
+                fill
+                className="object-cover hidden sm:block"
             />
-            <input
-              type="text"
-              placeholder="تلفن همراه"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className="flex-1 bg-[#f7f8fc] text-gray-700 rounded-lg p-2 px-4 text-sm border border-transparent focus:outline-none focus:border-[#1d546b] focus:ring-2 focus:ring-[#1d546b] transition-all duration-200"
+            {/* Mobile Image */}
+            <Image
+                src="/homepage/timeresmobile.png"
+                alt="Mobile slide"
+                fill
+                className="object-cover block sm:hidden"
             />
-            <motion.button
-              onClick={handleSubmit}
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              transition={{ type: 'spring', stiffness: 300 }}
-              className="w-32 h-10 bg-[#1d546b] cursor-pointer text-white rounded-xl whitespace-nowrap px-3"
-            >
-              {loading ? 'در حال ارسال...' : 'ثبت درخواست'}
-            </motion.button>
-          </div>
+            <div className="absolute top-50 md:top-6 right-4 items-start z-10 p-10 md:p-4 pt-10 rounded-xl">
+                <div className="mt-10">
+                    <p className="text-lg md:text-3xl text-white mb-4">
+                        قبل از ثبت نوبت می‌توانید از ما
+                        <span className="text-[#1d546b]"> مشاوره رایگان </span>
+                        دریافت کنید :
+                    </p>
+                </div>
+
+                {/* Input Fields */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full mt-10">
+                    <input
+                        type="text"
+                        placeholder="نام و نام خانوادگی"
+                        onChange={(e) => setName(e.target.value)}
+                        className="bg-[#f7f8fc] text-gray-700 rounded-lg p-2 px-4 text-sm border border-transparent focus:outline-none focus:border-[#1d546b] focus:ring-2 focus:ring-[#1d546b] transition-all duration-200"
+                    />
+                    <input
+                        type="text"
+                        placeholder="تلفن همراه"
+                        onChange={(e) => setPhone(e.target.value)}
+                        className="bg-[#f7f8fc] text-gray-700 rounded-lg p-2 px-4 text-sm border border-transparent focus:outline-none focus:border-[#1d546b] focus:ring-2 focus:ring-[#1d546b] transition-all duration-200"
+                    />
+                    <div className="w-full lg:w-auto">
+                       <motion.button
+                        onClick={handleSubmit}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        transition={{ type: 'spring', stiffness: 300 }}
+                        className="w-32 h-10 bg-[#1d546b] text-white rounded-xl whitespace-nowrap px-3"
+                      >
+                      {loading ? 'در حال ارسال...' : 'ثبت درخواست'}
+                    </motion.button>
+                  </div>
 
           {/* Success / Error messages */}
-          {success && <p className="text-green-500 mt-4">فرم با موفقیت ثبت شد!</p>}
-          {error && <p className="text-red-500 mt-4">{error}</p>}
-        </div>
-      </div>
-    </motion.div>
-  );
-}
+              {success && <p className="text-green-500 mt-4">فرم با موفقیت ثبت شد!</p>}
+              {error && <p className="text-red-500 mt-4">{error}</p>}
+            </div>
+          </div>
+          </div>
+        </motion.div>
+      );
+    }
